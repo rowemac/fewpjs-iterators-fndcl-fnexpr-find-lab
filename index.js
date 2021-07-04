ada10a616ee13c0d0265c747a077419ef227aeeb
Mac Rowe <m.rowe42@gmail.com>
2021-07-04 10:45:51 -0400

    commit tree fd8c3c7b145c051390aa0d363a5d530316dc9c01

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(record) {
+    let year = record.find(r => r.result === "W");
+    if (year) {
+        return year.year;
+    } else {
+        return undefined;
+    };
+};
